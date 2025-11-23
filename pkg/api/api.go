aeb7f635f08a0a1dce28bc8cd18214fe0a5a33e5
yeahgor <yeahgorQ@gmail.com>
2025-11-23 14:28:45 +0300

    обернул refresh в горутину, тем самым оптимизировав выполнение с 15 секунд до < 1 следующая щель допилить /health

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -4,6 +4,7 @@ import (
 	"GoNews/pkg/db"
 	"GoNews/pkg/models"
 	"GoNews/pkg/rss"
+	"GoNews/pkg/worker"
 	"net/http"
 	"strconv"
 
@@ -16,21 +17,21 @@ func StartServer(database *gorm.DB, cfg *models.Config, loader rss.RSSLoader) er
 
 	// GET /news?limit=10
 	router.GET("/news", func(c *gin.Context) {
-		limitStr := c.DefaultQuery("limit", "1")
+		limitStr := c.DefaultQuery("limit", "5")
 
 		n, err := strconv.Atoi(limitStr)
 		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "limit must be an integer"})
+			c.JSON(http.StatusBadRequest, gin.H{"Error": "limit must be an integer"})
 			return
 		}
 
 		if n <= 0 {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "limit must be > 0"})
+			c.JSON(http.StatusBadRequest, gin.H{"Error": "limit must be > 0"})
 			return
 		}
 		news, err := db.GetLastNews(database, n)
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to fetch news"})
+			c.JSON(http.StatusInternalServerError, gin.H{"Error": "failed to fetch news"})
 			return
 		}
 		c.JSON(http.StatusOK, news)
@@ -41,13 +42,13 @@ func StartServer(database *gorm.DB, cfg *models.Config, loader rss.RSSLoader) er
 		idStr := c.Param("id")
 		id, err := strconv.Atoi(idStr)
 		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "id must be an integer"})
+			c.JSON(http.StatusBadRequest, gin.H{"Error": "id must be an integer"})
 			return
 		}
 
 		news, err := db.GetNewsByID(database, id)
 		if err != nil {
-			c.JSON(http.StatusNotFound, gin.H{"error": "news not found"})
+			c.JSON(http.StatusNotFound, gin.H{"Error": "news not found"})
 			return
 		}
 		c.JSON(http.StatusOK, news)
@@ -55,12 +56,8 @@ func StartServer(database *gorm.DB, cfg *models.Config, loader rss.RSSLoader) er
 
 	// GET /news/refresh
 	router.GET("/news/refresh", func(c *gin.Context) {
-		err := rss.UpdateAll(cfg.RSS, loader, database)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to refresh RSS"})
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+		go worker.StartWorkerPool(cfg, database, loader)
+		c.JSON(http.StatusOK, gin.H{"Status": "refresh started"})
 	})
 	//health
 	return router.Run(":8080")
